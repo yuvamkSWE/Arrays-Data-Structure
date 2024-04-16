b66726b97e1252def24773efc1640e10cc81c2b4
Yuvam Kumar <yuvamk.swe@gmail.com>
2024-04-15 23:09:08 -0500

    New diagonal sum class

---
+++ Basic Algorithms/src/Diagonal_Sum.java
@@ -0,0 +1,27 @@
+// Given 2D array calculate the sum of diagonal elements.
+
+public class Diagonal_Sum {
+    public static void main(String[] args) {
+        int[][] testArray = {{1,2,3},{4,5,6},{7,8,9}};
+        diagonal(testArray);
+    }
+
+    public static void diagonal(int[][] array){
+
+        int sum = 0;
+
+        for (int i = 0 ; i < array.length ; i++){
+            for (int j = 0 ; j < array[i].length ; j++){
+
+                if (i == j){
+                    sum += array[i][j];
+                }
+
+            }
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
